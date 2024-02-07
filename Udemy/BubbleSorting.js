11a40af59102c1402b351adc61b827be2b06f9eb
Andy Lin <ld130102@gmail.com>
2024-02-07 17:25:06 +0800

    [FEAT] Optimize bubble sort code

--- Udemy/BubbleSorting.js
+++ Udemy/BubbleSorting.js
@@ -1,12 +1,17 @@
 function bubbleSort(arr) {
   for (let i = 0; i <= arr.length - 2; i++) {
+    let swapping = false;
     for (let j = arr.length - 1; j >= i + 1; j--) {
       if (arr[j] < arr[j - 1]) {
         // swap arr[j] and arr[j-1]
         let temp = arr[j];
         arr[j] = arr[j - 1];
         arr[j - 1] = temp;
+        swapping = true;
       }
+    }
+    if (swapping === false) {
+      break;
     }
   }
   return arr;
