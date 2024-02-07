6becae62411830f35435def8105a5c254cf6868f
Andy Lin <ld130102@gmail.com>
2024-02-07 17:23:56 +0800

    [FEAT] Bubble sorting code

---
+++ Udemy/BubbleSorting.js
@@ -0,0 +1,15 @@
+function bubbleSort(arr) {
+  for (let i = 0; i <= arr.length - 2; i++) {
+    for (let j = arr.length - 1; j >= i + 1; j--) {
+      if (arr[j] < arr[j - 1]) {
+        // swap arr[j] and arr[j-1]
+        let temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([4, 1, 5, 2, 7]));
